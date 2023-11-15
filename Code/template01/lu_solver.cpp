addcd4a831aec829f22299bda6d48e0ae5a2b7c4
Samuel <57289590+Fettmann@users.noreply.github.com>
2023-11-15 12:28:15 +0100

    rein da

--- Code/template01/lu_solver.cpp
+++ Code/template01/lu_solver.cpp
@@ -23,24 +23,27 @@ LUSolver<TMatrix>::LUSolver(const matrix_type& mat)
 {
     //mSolution = Vector(mat.m_cols*mat.m_rows);				// mat ist A
 
-	size_t size = mat.columns;
-	Vector LU = mat;  // Initialize LU with the original matrix A
+	size_t size = mat.m_cols;
+	Vector LU = mat;  									// Initialize LU with the original matrix A
 
 	// Perform LU decomposition
-	for (int k = 0; k < size - 1; ++k) {			// Zähler k über Matrixgröße
-		for (int i = k + 1; i < size; ++i) {		// für jedes k, ein Zähler i über die restliche Matrixgröße
+	for (int k = 0; k < mat.m_cols - 1; ++k) {			// Zähler k über Matrixgröße
+		for (int i = k + 1; i < mat.m_rows; ++i) {		// für jedes k, ein Zähler i über die restliche Matrixgröße
 
-			if (LU[k][k] == 0.0) {
+			size_t comparisionEntry = k * mat.m_cols + k * mat.m_rows;			// next entry to compare to and calculate factor
+
+			if (LU[comparisionEntry] == 0.0) {
 				std::cerr << "LU decomposition failed: Division by zero." << std::endl;
 				return;
 			}
 
-			double factor = LU[i][k] / LU[k][k];	// berechne Faktor für L/U
-			LU[i][k] = factor;
+			double factor = LU[(i*mat.m_cols)+k] / LU[comparisionEntry];		// berechne Faktor für L/U
+			LU[(i*mat.m_cols)+k] = factor;										// schreibe Faktor an die Stelle
 
-			for (int j = k + 1; j < size; ++j) {
-				LU[i][j] -= factor * LU[k][j];
 			}
+		for (int j = 0; j < k + 1; j++) {		// wende die Änderung auf die Matrix an und berechne R-Matrix Werte
+			LU[comparisionEntry + j] -= factor * LU[comparisionEntry - mat.m_cols];
+
 		}
 	}
 }
@@ -73,3 +76,6 @@ bool LUSolver<TMatrix>::solve(vector_type& x, const vector_type& b) const
 template class LUSolver<Matrix>;
 template class LUSolver<SparseMatrix>;
 
+int main(){
+	return 0;
+}
